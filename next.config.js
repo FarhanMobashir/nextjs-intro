9d829174b53335f830ab75e3d995d341a51a3197
Mobashir Farhan <farhanmobashir007@gmail.com>
2021-09-24 19:06:00 +0530

    completed the course and deploys the project to vercel

--- next.config.js
+++ next.config.js
@@ -1,3 +1,11 @@
+const nextEnv = require("next-env");
+const dotenvLoad = require("dotenv-load");
+
+dotenvLoad();
+
+const withNextEnv = nextEnv();
+module.exports = withNextEnv();
+
 module.exports = {
   reactStrictMode: true,
-}
+};
---
+++ src/data/data.js
@@ -0,0 +1,21 @@
+// src/data/data
+const notes = [
+  {
+    id: 1,
+    title: "Note 1",
+  },
+  {
+    id: 2,
+    title: "Note 2",
+  },
+  {
+    id: 3,
+    title: "Note 3",
+  },
+  {
+    id: 4,
+    title: "Note 4",
+  },
+];
+
+module.exports = notes;
